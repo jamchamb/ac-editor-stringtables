3735f304e6ee1ff25938469fdbd09b8d08190121
James Chambers <jameschambers2@gmail.com>
2018-04-15 20:09:36 -0400

    Reorganizing some string table logic

--- src/main/kotlin/controllers/StringTableController.kt
+++ src/main/kotlin/controllers/StringTableController.kt
@@ -1,11 +1,9 @@
 package controllers
 
-import com.sun.javaws.exceptions.InvalidArgumentException
 import javafx.collections.FXCollections
 import models.StringTable
 import models.StringTableEntry
 import tornadofx.Controller
-import java.io.*
 
 class StringTableController: Controller() {
     var stringTable = StringTable()
@@ -13,17 +11,17 @@ class StringTableController: Controller() {
     var tableChanged = false
 
     fun loadTable (tablePath: String, dataPath: String) {
-        val result: StringTable = loadTableFromFiles(tablePath, dataPath)
-        // TODO hacky
-        stringTable.endingsTableSlots = result.endingsTableSlots
-        stringTable.dataFileSize = result.dataFileSize
-        stringTable.endingsEmptySlots = result.endingsEmptySlots
+        // TODO Hacky
+        stringTable.loadTableFromFiles(tablePath, dataPath)
         stringTableEntries.clear()
-        stringTableEntries.addAll(result)
+        stringTableEntries.addAll(stringTable.entries)
     }
 
     fun saveTable (tablePath: String, dataPath: String) {
-        saveTableToFiles(tablePath, dataPath)
+        // TODO Hacky
+        stringTable.entries.clear()
+        stringTable.entries.addAll(stringTableEntries)
+        stringTable.saveTableToFiles(tablePath, dataPath)
     }
 
     fun closeTable () {
@@ -32,121 +30,5 @@ class StringTableController: Controller() {
         }
         stringTable = StringTable()
         stringTableEntries.clear()
-    }
-
-    private fun loadTableFromFiles (tablePath: String, dataPath: String): StringTable {
-        val tableInputStream = File(tablePath).inputStream()
-        val tableBufferedInputStream = BufferedInputStream(tableInputStream)
-        val tableDataInputStream = DataInputStream(tableBufferedInputStream)
-
-        val endingsTable = ArrayList<Int>()
-
-        // Read big endian ints from end position table
-        while (tableDataInputStream.available() > 0) {
-            val endingPos = tableDataInputStream.readInt()
-            // TODO Make sure these read as unsigned 32 bit ints
-            if (endingPos < 0) {
-                error("Negative ending position: $endingPos")
-            }
-            endingsTable.add(endingPos)
-        }
-
-        tableInputStream.close()
-        tableBufferedInputStream.close()
-        tableDataInputStream.close()
-
-        // Set up a table object
-        val result = StringTable()
-
-        // Read in the string data
-        val dataInStream = File(dataPath).inputStream()
-        val dataBufferedInStream = BufferedInputStream(dataInStream)
-        val dataDataInputStream = DataInputStream(dataBufferedInStream)
-
-        // Get total size of data file. Warn the user if this is exceeded upon save.
-        result.dataFileSize = dataInStream.channel.size()
-
-        var pos = 0
-        for ((index, endingPos) in endingsTable.withIndex()) {
-            if (pos > 0 && endingPos == 0) {
-                // Zero values are used for empty entries at the end
-                break
-            } else if (endingPos < pos) {
-                throw InvalidArgumentException(arrayOf("File string positions not in ascending order ($endingPos < $pos)"))
-            }
-
-            // Get the string bytes
-            val length = endingPos - pos
-            val byteArray = ByteArray(length)
-            dataDataInputStream.read(byteArray)
-
-            // Create new entry with raw bytes
-            result.add(StringTableEntry(index, byteArray))
-
-            pos = endingPos
-        }
-
-        dataInStream.close()
-        dataBufferedInStream.close()
-        dataDataInputStream.close()
-
-        // Save total number of end position slots. Warn user if total number of slots or number of used slots increases.
-        result.endingsTableSlots = endingsTable.size
-        result.endingsEmptySlots = endingsTable.size - result.size
-
-        return result
-    }
-
-    private fun saveTableToFiles(tablePath: String, dataPath: String) {
-        // Collect data to write to files
-        var endPosition = 0
-
-        val endingsTable = ArrayList<Int>(stringTable.endingsTableSlots)
-        val dataTable = ArrayList<Byte>(stringTableEntries.size)
-
-        // Testing encoders
-        for (entry in stringTableEntries) {
-            val entryBytes = entry.encodeMessage()
-            endPosition += entryBytes.size
-            endingsTable.add(endPosition)
-            dataTable.addAll(entryBytes.toList())
-        }
-
-        // Add filler data
-        var i = 0
-        while (i < stringTable.endingsEmptySlots) {
-            endingsTable.add(0)
-            i++
-        }
-
-        while (dataTable.size < stringTable.dataFileSize) {
-            dataTable.add(0x00.toByte())
-        }
-
-        // Table output stream
-        val tableOutputStream = File(tablePath).outputStream()
-        val tableBufferedOutputStream = BufferedOutputStream(tableOutputStream)
-        val tableDataOutputStream = DataOutputStream(tableBufferedOutputStream)
-
-        // Data output stream
-        val dataOutputStream = File(dataPath).outputStream()
-        val dataBufferedOutputStream = BufferedOutputStream(dataOutputStream)
-
-        // Write the table
-        for (ending in endingsTable) {
-            tableDataOutputStream.writeInt(ending)
-        }
-
-        // Write the data
-        dataBufferedOutputStream.write(dataTable.toByteArray())
-
-        // Close data file
-        dataBufferedOutputStream.close()
-        dataOutputStream.close()
-
-        // Close table file
-        tableDataOutputStream.close()
-        tableBufferedOutputStream.close()
-        tableOutputStream.close()
     }
 }
--- src/main/kotlin/models/StringTable.kt
+++ src/main/kotlin/models/StringTable.kt
@@ -1,7 +1,124 @@
 package models
 
-class StringTable: ArrayList<StringTableEntry>() {
+import com.sun.javaws.exceptions.InvalidArgumentException
+import java.io.*
+
+class StringTable {
     var endingsTableSlots: Int = 0
     var endingsEmptySlots: Int = 0
     var dataFileSize: Long = 0
+    var entries = ArrayList<StringTableEntry>()
+
+    fun loadTableFromFiles (tablePath: String, dataPath: String) {
+        entries.clear()
+
+        val tableInputStream = File(tablePath).inputStream()
+        val tableBufferedInputStream = BufferedInputStream(tableInputStream)
+        val tableDataInputStream = DataInputStream(tableBufferedInputStream)
+
+        val endingsTable = ArrayList<Int>()
+
+        // Read big endian ints from end position table
+        while (tableDataInputStream.available() > 0) {
+            val endingPos = tableDataInputStream.readInt()
+            // TODO Make sure these read as unsigned 32 bit ints
+            if (endingPos < 0) {
+                error("Negative ending position: $endingPos")
+            }
+            endingsTable.add(endingPos)
+        }
+
+        tableInputStream.close()
+        tableBufferedInputStream.close()
+        tableDataInputStream.close()
+
+        // Read in the string data
+        val dataInStream = File(dataPath).inputStream()
+        val dataBufferedInStream = BufferedInputStream(dataInStream)
+        val dataDataInputStream = DataInputStream(dataBufferedInStream)
+
+        // Get total size of data file. Warn the user if this is exceeded upon save.
+        dataFileSize = dataInStream.channel.size()
+
+        var pos = 0
+        for ((index, endingPos) in endingsTable.withIndex()) {
+            if (pos > 0 && endingPos == 0) {
+                // Zero values are used for empty entries at the end
+                break
+            } else if (endingPos < pos) {
+                throw InvalidArgumentException(arrayOf("File string positions not in ascending order ($endingPos < $pos)"))
+            }
+
+            // Get the string bytes
+            val length = endingPos - pos
+            val byteArray = ByteArray(length)
+            dataDataInputStream.read(byteArray)
+
+            // Create new entry with raw bytes
+            entries.add(StringTableEntry(index, byteArray))
+
+            pos = endingPos
+        }
+
+        dataInStream.close()
+        dataBufferedInStream.close()
+        dataDataInputStream.close()
+
+        // Save total number of end position slots. Warn user if total number of slots or number of used slots increases.
+        endingsTableSlots = endingsTable.size
+        endingsEmptySlots = endingsTable.size - entries.size
+    }
+
+    fun saveTableToFiles(tablePath: String, dataPath: String) {
+        // Collect data to write to files
+        var endPosition = 0
+
+        val endingsTable = ArrayList<Int>(endingsTableSlots)
+        val dataTable = ArrayList<Byte>(entries.size)
+
+        // Testing encoders
+        for (entry in entries) {
+            val entryBytes = entry.encodeMessage()
+            endPosition += entryBytes.size
+            endingsTable.add(endPosition)
+            dataTable.addAll(entryBytes.toList())
+        }
+
+        // Add filler data
+        var i = 0
+        while (i < endingsEmptySlots) {
+            endingsTable.add(0)
+            i++
+        }
+
+        while (dataTable.size < dataFileSize) {
+            dataTable.add(0x00.toByte())
+        }
+
+        // Table output stream
+        val tableOutputStream = File(tablePath).outputStream()
+        val tableBufferedOutputStream = BufferedOutputStream(tableOutputStream)
+        val tableDataOutputStream = DataOutputStream(tableBufferedOutputStream)
+
+        // Data output stream
+        val dataOutputStream = File(dataPath).outputStream()
+        val dataBufferedOutputStream = BufferedOutputStream(dataOutputStream)
+
+        // Write the table
+        for (ending in endingsTable) {
+            tableDataOutputStream.writeInt(ending)
+        }
+
+        // Write the data
+        dataBufferedOutputStream.write(dataTable.toByteArray())
+
+        // Close data file
+        dataBufferedOutputStream.close()
+        dataOutputStream.close()
+
+        // Close table file
+        tableDataOutputStream.close()
+        tableBufferedOutputStream.close()
+        tableOutputStream.close()
+    }
 }
